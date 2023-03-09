331bf07c74a9eff2d0cab130e6fdde84a42f0f83
CristiDS-Work <126245567+CristiDS-Work@users.noreply.github.com>
2023-03-09 10:21:07 +0200

    Add files via upload

---
+++ Visual Studio/src/Feature/Blog/code/Controllers/BlogListController.cs
@@ -0,0 +1,89 @@
+﻿using Blog.Feature.Blog.Models;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.Linq.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Blog.Feature.Blog.Controllers
+{
+    public class BlogListController : Controller
+    {
+        // GET: BlogList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DoSearch(string searchText)
+        {
+
+            var myResults = new SearchResults
+            {
+                Results = new List<SearchResult>()
+            };
+            var searchIndex = ContentSearchManager.GetIndex("sitecore_web_index"); // Get the search index
+            var searchPredicate = GetSearchPredicate(searchText); // Build the search predicate
+            using (var searchContext = searchIndex.CreateSearchContext()) // Get a context of the search index
+            {
+                //Select * from Sitecore_web_index Where Author="searchText" OR Description="searchText" OR Title="searchText"
+                //var searchResults = searchContext.GetQueryable<SearchModel>().Where(searchPredicate); // Search the index for items which match the predicate
+                var searchResults = searchContext.GetQueryable<BlogSearchModel>()
+                    .Where(x => x.TemplateName == "BlogArticle" && x.ItemName != "Home" && x.ItemName != "__Standard Values" && x.ItemName != "404" && (x.Title.Contains(searchText) || x.Category.Contains(searchText) || x.ShortText.Contains(searchText) || x.LongText.Contains(searchText) || x.Tags.Contains(searchText)));   //LINQ query
+
+                var fullResults = searchResults.GetResults();
+
+                // This is better and will get paged results - page 1 with 10 results per page
+                //var pagedResults = searchResults.Page(1, 10).GetResults();
+                foreach (var hit in fullResults.Hits)
+                {
+                    var item_uniqueid = hit.Document.ItemPath;
+                    var cutFrom = item_uniqueid.IndexOf("{");
+                    var cutTo = item_uniqueid.LastIndexOf("}") + 1;
+                    var item_id = item_uniqueid.Substring(cutFrom, cutTo - cutFrom);
+
+                    var item = Sitecore.Context.Database.GetItem(item_id);
+                    string item_url = Sitecore.Links.LinkManager.GetItemUrl(item);
+
+                    myResults.Results.Add(new SearchResult
+                    {
+                        Tags = hit.Document.Tags,
+                        LongText = hit.Document.LongText,
+                        ShortText = hit.Document.ShortText,
+                        BigImage = hit.Document.BigImage,
+                        SmallImage = hit.Document.SmallImage,
+                        Date = hit.Document.Date.ToString("MMM d, yyyy"),
+                        Category = hit.Document.Category,
+                        Title = hit.Document.Title,
+                        ItemPath = item_url,
+                    });
+                }
+                return new JsonResult { JsonRequestBehavior = JsonRequestBehavior.AllowGet, Data = myResults };
+            }
+        }
+
+        /// <summary>
+        /// Search logic
+        /// </summary>
+        /// <param name="searchText">Search term</param>
+        /// <returns>Search predicate object</returns>
+        public static Expression<Func<BlogSearchModel, bool>> GetSearchPredicate(string searchText)
+        {
+            var predicate = PredicateBuilder.True<BlogSearchModel>(); // Items which meet the predicate
+                                                                  // Search the whole phrase - LIKE
+                                                                  // predicate = predicate.Or(x => x.DispalyName.Like(searchText)).Boost(1.2f);
+                                                                  // predicate = predicate.Or(x => x.Description.Like(searchText)).Boost(1.2f);
+                                                                  // predicate = predicate.Or(x => x.Title.Like(searchText)).Boost(1.2f);
+                                                                  // Search the whole phrase - CONTAINS
+            predicate = predicate.Or(x => x.LongText.Contains(searchText)); // .Boost(2.0f);
+            predicate = predicate.Or(x => x.ShortText.Contains(searchText)); // .Boost(2.0f);
+            predicate = predicate.Or(x => x.Category.Contains(searchText)); // .Boost(2.0f);
+            predicate = predicate.Or(x => x.Title.Contains(searchText)); // .Boost(2.0f);
+            //Where Author="searchText" OR Description="searchText" OR Title="searchText"
+            return predicate;
+        }
+    }
+}
