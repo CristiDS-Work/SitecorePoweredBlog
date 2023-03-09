bfa2395cb116a5ca20203c6f1eb8d1bacf09f59a
CristiDS-Work <126245567+CristiDS-Work@users.noreply.github.com>
2023-03-09 10:19:25 +0200

    Add files via upload

---
+++ Visual Studio/src/Feature/Blog/code/HttpRequestProcessor404.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Blog.Feature.Blog
+{
+    public class HttpRequestProcessor404 : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (Sitecore.Context.Item != null || Sitecore.Context.Site == null || Sitecore.Context.Database == null
+                || Sitecore.Context.Database.Name == "core"
+                || args.RequestUrl.AbsoluteUri.ToLower().Contains("/sitecore/api/layout/render/jss")
+                || args.RequestUrl.AbsoluteUri.ToLower().Contains("/sitecore/api/jss/import")
+                || args.RequestUrl.AbsoluteUri.ToLower().Contains("/api/sitecore/"))
+            {
+                return;
+            }
+            var pageNotFound = Sitecore.Context.Database.GetItem(@"{E6571ADC-8421-4B27-8BDC-1C51E3FCFA6E}");
+            if (pageNotFound == null)
+                return;
+            args.ProcessorItem = pageNotFound;
+            Sitecore.Context.Item = pageNotFound;
+            args.HttpContext.Response.StatusCode = 404;
+        }
+    }
+}
